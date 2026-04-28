f1435c57a3d9f5883c13134e231216f8c357ae73
neda-jahadi <neda_jahadi@yahoo.com>
2026-04-28 23:54:43 +0200

    Added validation with zod for validating body of requests

--- server/routes/watchlistRoutes.js
+++ server/routes/watchlistRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { addToWatchlist } from '../controllers/watchlistController.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
+import { validateRequest } from '../middlewares/validateRequest.js';
+import { addToWatchlistSchema } from '../validators/watchlistValidators.js';
 
 const router = express.Router();
 router.use(authMiddleware);
 
-router.post("/", addToWatchlist);
+router.post("/",validateRequest(addToWatchlistSchema), addToWatchlist);
 
 export default router;
